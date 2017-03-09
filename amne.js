b462ef263b8369e8139476609bbacb7836033543
Tin <tinytheyfon8@gmail.com>
2017-03-08 18:17:22 -0600

    Promises set up. Creating calcResult function

--- amne.js
+++ amne.js
@@ -9,6 +9,8 @@
  5 3
   188930 194123 201345 154243 154243
 
+
+Window = N-K+1
 */
 
 // var inputData
@@ -17,21 +19,73 @@
 //Read the file and saving it to the scope
 var express = require('express');
 var fs = require('fs');
-// var promise = readFile();
-// var q = require('q');
 
-var readFile = function() {
+
+// var readFile = function() {
+//   return new Promise(function(resolve, reject) {
+//     fs.readFile('input.txt', 'utf8', function(err, data) {
+//       if (err) {
+//         return console.log(err);
+//       }
+//       const inputData = data;
+//       console.log(inputData);
+//       return inputData;
+//     })
+//   })
+// }
+
+fs.readFileAsync = function(filename) {
   return new Promise(function(resolve, reject) {
-    fs.readFile('input.txt', 'utf8', function(err, data) {
+    fs.readFile(filename, 'utf8', function(err, data) {
       if (err) {
-        return console.log(err);
+       // console.log('err:', err);
+        reject(err);
       }
-      const inputData = data;
-      console.log(inputData);
-      return inputData;
+        // console.log('data:', data);
+        // return data;
+        resolve(data);
     })
   })
 }
+
+var calcResult = function(data) {
+
+  var input = data.split('\n');
+  //I need to convert these to ints
+  var firstLine = input[0].split(' ');
+  for (var i = 0; i< firstLine.length; i++) {
+    firstLine[i] = parseInt(firstLine[i], 10);
+  }
+
+  var averagePrice = input[1].split(' ');
+  for (var i = 0; i< averagePrice.length; i++) {
+    averagePrice[i] = parseInt(averagePrice[i], 10);
+  }
+
+  // var numDays = inputs[0];
+  let result = 0;
+  // const windowNum = firstLine[1];
+  const windowNum = firstLine[0] - firstLine[1] + 1;
+
+  console.log(firstLine);
+  console.log(averagePrice);
+  console.log(windowNum);
+
+  for (var i = 0; i < windowNum; i++) {
+    if (averagePrice[i] < averagePrice[i+1]) {
+      console.log('index:', i);
+      console.log('ran');
+      result ++;
+    } else if(averagePrice[i] > averagePrice[i+1]) {
+      console.log('less than ran');
+      result --;
+    }
+  }
+
+  console.log('Result:', result);
+  return result;
+}
+
 
 // function readFile() {
 //   fs.readFile('input.txt', 'utf8', function(err, data) {
@@ -46,13 +100,24 @@ var readFile = function() {
 // readFile.then(function() {console.log('erjaskdas')
 // });
 
-readFile().then(function(err, data) {
-  return console.log('sadasda');
-}).catch(function(error) {
-  return console.log('oh no!');
-}).then(function() {
-  return console.log('carry on');
-})
+// readFile().then(function(err, data) {
+//   return console.log('sadasda');
+// }).catch(function(error) {
+//   return console.log('oh no!');
+// }).then(function() {
+//   return console.log('carry on');
+// })
+
+fs.readFileAsync('input.txt')
+  .then(function(data) {
+  // const inputData = data;
+  calcResult(data);
+  console.log('inputdata:', data);
+}).catch(function(err) {
+  if (err) {
+    console.log('err', err);
+  };
+});
 
 console.log('End of the script');
 
